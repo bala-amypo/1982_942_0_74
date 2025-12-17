42cb7df9190cdb7ed2b5691e7722d183bd08467d
bala-amypo <bala@amypo.in>
2025-12-17 05:07:58 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Stuentitty.java
@@ -0,0 +1,50 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Stuentity{
+    @id
+    @GenerativeValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private float cgpa;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public float getCgpa(){
+        return cgpa;
+    }
+    public void setCgpa(){
+        this.cgpa=cgpa;
+    }
+    public Stuentity(Long id,String name,String email,float cgpa){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.cgpa=cgpa;
+    }
+    public Stuentity(){
+        
+    }
+}
